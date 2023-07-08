412ab25a28e7921c5813e30b9bac0f5e9701caf9
Andrew Finogenov <avfinogenov@gmail.com>
2023-07-08 19:50:48 +0300

    add selection sort

--- SortingOfAllKind/SortingOfAllKind/SortingOfAllKind.cpp
+++ SortingOfAllKind/SortingOfAllKind/SortingOfAllKind.cpp
@@ -2,11 +2,12 @@
 #include <vector>
 #include "arrays.h"
 
-void bubbleSort(std::vector<int> &input);
+void bubbleSort(std::vector<int>& input);
+void selectionSort(std::vector<int>& input);
 
 int main()
 {
-    std::vector<int> forSort[15]{arr1, arr2, arr3, arr4, arr5, arr6, arr7, arr8, arr9, arr10, arr11, arr12, arr13, arr14, arr15};
+    std::vector<int> forSort[15]{ arr1, arr2, arr3, arr4, arr5, arr6, arr7, arr8, arr9, arr10, arr11, arr12, arr13, arr14, arr15 };
 
     for (int i = 0; i < 15; ++i)
     {
@@ -17,7 +18,8 @@ int main()
             std::cout << forSort[i][j] << " ";
         }
         std::cout << std::endl;
-        bubbleSort(forSort[i]);
+        //bubbleSort(forSort[i]);
+        selectionSort(forSort[i]);
         for (int j = 0; j < forSort[i].size(); ++j)
         {
             std::cout << forSort[i][j] << " ";
@@ -45,7 +47,7 @@ int main()
 }
 
 
-void bubbleSort(std::vector<int> &input)
+void bubbleSort(std::vector<int>& input)
 {
     for (int i = 0; i < input.size(); ++i)
     {
@@ -60,6 +62,28 @@ void bubbleSort(std::vector<int> &input)
 
         }
     }
+
+
+}
+
+void selectionSort(std::vector<int>& input)
+{
+    for (int i = 0; i < input.size(); ++i)
+    {
+        int min = input[i];
+        for (int j = i; j < input.size(); ++j)
+        {
+            if (input[j] < min)
+            {
+                min = input[j];
+            }
+
+        }
+        input[i] = min;
+    }
+
+
+
 
 
 }
