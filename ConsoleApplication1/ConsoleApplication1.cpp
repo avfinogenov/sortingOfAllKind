05cd69239efef8564edc56b00152bc6ea9583be2
Andrew Finogenov <avfinogenov@gmail.com>
2023-07-08 19:36:39 +0300

    start with this

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,65 @@
+﻿#include <iostream>
+#include <vector>
+#include "arrays.h"
+
+void bubbleSort(std::vector<int> &input);
+
+int main()
+{
+    std::vector<int> forSort[15]{arr1, arr2, arr3, arr4, arr5, arr6, arr7, arr8, arr9, arr10, arr11, arr12, arr13, arr14, arr15};
+
+    for (int i = 0; i < 15; ++i)
+    {
+        std::vector<int> asd(forSort[i]);
+        asd = forSort[i];
+        for (int j = 0; j < forSort[i].size(); ++j)
+        {
+            std::cout << forSort[i][j] << " ";
+        }
+        std::cout << std::endl;
+        bubbleSort(forSort[i]);
+        for (int j = 0; j < forSort[i].size(); ++j)
+        {
+            std::cout << forSort[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+
+
+    for (int i = 0; i < 15; ++i)
+    {
+        for (int j = 0; j < forSort[i].size() - 1; ++j)
+        {
+            if (forSort[i][j] > forSort[i][j + 1])
+            {
+                std::cout << "wrong at " << i << " element " << j << std::endl;
+                return 0;
+            }
+        }
+    }
+    std::cout << "test succes\n";
+
+
+    std::cout << "Hello world!" << std::endl;
+    return 0;
+}
+
+
+void bubbleSort(std::vector<int> &input)
+{
+    for (int i = 0; i < input.size(); ++i)
+    {
+        for (int j = 0; j < input.size() - 1; ++j)
+        {
+            if (input[j] > input[j + 1])
+            {
+                int tmp = input[j];
+                input[j] = input[j + 1];
+                input[j + 1] = tmp;
+            }
+
+        }
+    }
+
+
+}
---
+++ ConsoleApplication1/arrays.h
@@ -0,0 +1,32 @@
+#ifndef ARRAYS_H
+#define ARRAYS_H
+#include <iostream>
+#include <vector>
+
+
+std::vector<int> arr1{5, 2, 9, 1, 3};
+std::vector<int> arr2{10, 4, 7, 6, 8};
+std::vector<int> arr3{3, 8, 2, 6, 1, 9, 4};
+
+std::vector<int> arr4{1, 2, 3, 4, 5};
+std::vector<int> arr5{10, 20, 30, 40, 50};
+std::vector<int> arr6{100, 200, 300, 400, 500};
+
+std::vector<int> arr7{9, 7, 5, 3, 1};
+std::vector<int> arr8{50, 40, 30, 20, 10};
+std::vector<int> arr9{500, 400, 300, 200, 100};
+
+
+std::vector<int> arr10{2, 5, 3, 2, 1, 4, 2};
+std::vector<int> arr11{10, 4, 7, 6, 8, 4, 10};
+std::vector<int> arr12{3, 8, 2, 6, 1, 9, 4, 3, 2, 8};
+
+std::vector<int> arr13{5, -2, 9, -1, 3};
+std::vector<int> arr14{-10, 4, -7, 6, -8};
+std::vector<int> arr15{3, -8, 2, -6, 1, -9, 4};
+
+
+
+
+
+#endif // ARRAYS_H
